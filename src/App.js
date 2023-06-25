9cfa5cb0e1b162dd889620272cf9589e9a0cd114
PRIYANSHURAJROY4U <priyanshur.ip.21@nitj.ac.in>
2023-06-25 16:32:51 +0530

    first add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
+import React, {useState} from 'react';
+
 
 function App() {
+  const productList = [
+    {
+      price:150000,
+      name : "Iphone 12 pro",
+      qty: 0,
+    },
+    {
+      price:15000,
+      name : "Redmi 12 pro",
+      qty: 0,
+    }
+  ]
+
+     [productList , setProductList] = useState([]);
+
+   const incrementquantity = (index) => {
+    let newproductList = [...productList]
+    newproductList[index].qty++
+    setProductlist(newproductList);
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <main className='container mt-6r'> 
+    <ProductList productList={productList}  incrementquantity={incrementquantity}/>
+    </main>
+     </>
   );
 }
 
---
+++ src/Components/Product.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Product(props) {
+  return (
+    <div className="row">
+      <div className="col-5">
+        <h2>
+          {props.product.name}
+          <span class="badge bg-secondary"> ₹{props.product.price}</span>
+        </h2>
+      </div>
+
+      <div className="col-3">
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn bg-secondary">
+            Left
+          </button>
+          <button type="button" class="btn btn-secondary">
+            {props.product.qty}
+          </button>
+          <button type="button" class="btn bg-secondary" onClick={() => {
+             props.incrementquantity(props.index);
+          }} >
+            Right
+          </button>
+        </div>
+      </div>
+       
+       
+       <div className="col-4">
+        {props.product.qty*props.product.price}
+
+
+       </div>
+
+    </div>
+  );
+}
